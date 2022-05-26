c9d3528552a99da3773656b3ecb36d4e9462e822
zahidmiran <zahidmiron599@gmail.com>
2022-05-26 21:35:27 +0600

    components added

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+     <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga assumenda architecto eligendi aspernatur, quidem pariatur obcaecati ducimus, dolor ipsum ipsa alias quasi distinctio delectus temporibus quisquam rem accusamus, in veritatis?</h1>
     </div>
   );
 }
