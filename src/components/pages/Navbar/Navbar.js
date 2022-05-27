1e03acf6bb7cb8c97d1bcac71602ca470a813329
zahidmiran <zahidmiron599@gmail.com>
2022-05-27 10:07:42 +0600

    user admin product added done

--- src/components/pages/Navbar/Navbar.js
+++ src/components/pages/Navbar/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = ({ children }) => {
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
             </svg>
           </label>}
-          <div class='flex-1 px-2 mx-2 text-2xl'><Link to='/'>RIPARA..</Link> </div>
+          <div class='flex-1 px-2 mx-2 text-2xl'><Link to='/'>Tools BD..</Link> </div>
           <div class='flex-none lg:hidden'>
             <label for='my-drawer-3' class='btn btn-square btn-ghost'>
               <svg
